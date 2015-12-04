9dd0edc3387cb18553d5335e98a2c886fb3a6b28
rexcode <vickympatel@gmail.com>
2015-12-04 14:02:43 +0530

    bg css changes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,47 +12,48 @@
 	{{ HTML::style('css/main.css') }}
  </head>
 <body>
-		
-<div class="navbar navbar-inverse navbar-fixed-top">
-	<div class="container">
-		<a class="navbar-brand" href="/todos">ListApp</a>
-		<ul class="nav navbar-nav navbar-right">
-			
-			@if(Auth::check())
-				<li><a href="/todos">My Lists</a></li>
-				<li><a href="/todos/create">Create a new List</a></li>
-				<li><a href="/logout">Logout ({{{ Auth::user()->username }}})</a></li>
-			@endif
+<div class="bg">		
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="/todos">ListApp</a>
+			<ul class="nav navbar-nav navbar-right">
+				
+				@if(Auth::check())
+					<li><a href="/todos">My Lists</a></li>
+					<li><a href="/todos/create">Create a new List</a></li>
+					<li><a href="/logout">Logout ({{{ Auth::user()->username }}})</a></li>
+				@endif
 
-			@if(Auth::guest())
-				{{-- <li><a href="/">Home</a></li> --}}
-				<li><a href="/register">Register</a></li>
-				<li><a href="/login">Login</a></li>
-			@endif
+				@if(Auth::guest())
+					{{-- <li><a href="/">Home</a></li> --}}
+					<li><a href="/register">Register</a></li>
+					<li><a href="/login">Login</a></li>
+				@endif
 
-		</ul>
-	</div>
-</div>
-
-	@if(Session::has('message'))
-	<div class=" text-center alert alert-success alert-dismissable">
-		{{ Session::get('message') }}
-	</div>
-	@endif
-
-<div class="container border">
-		<div class="row">
-			<div class="col-md-12 content">
-				@yield('content')
-			</div>
+			</ul>
 		</div>
-</div>
+	</div>
 
-<div class="footer text-center">
-	<span>&copy; - {{{ date('Y', time()) }}}</span>
-	<a href="https://rexcode.github.io" target="_blank" title="Github Page">Rexcode</a>
-	<a href="https://github.com/rexcode/listapp/" target="_blank"><img src="{{ asset('images/github10.png') }}" alt="Github icon" title="Github repository of this project " width="12px" height="12px"></a>
+		@if(Session::has('message'))
+		<div class=" text-center alert alert-success alert-dismissable">
+			{{ Session::get('message') }}
+		</div>
+		@endif
+
+	<div class="container border">
+			<div class="row">
+				<div class="col-md-12 content">
+					@yield('content')
+				</div>
+			</div>
+	</div>
 </div>
+	<div class="footer text-center">
+		<span>&copy; - {{{ date('Y', time()) }}}</span>
+		<a href="https://rexcode.github.io" target="_blank" title="Github Page">Rexcode</a>
+		<a href="https://github.com/rexcode/listapp/" target="_blank"><img src="{{ asset('images/github10.png') }}" alt="Github icon" title="Github repository of this project " width="12px" height="12px"></a>
+	</div>
+
 
 	{{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js') }}
 	{{ HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js') }}
